e7793874fde25e4fa7fcda06e95ce5dce70dfcdd
Nahi-Terefe <nahi.terefe@gmail.com>
2021-08-26 22:31:53 +0300

    add 4-puts script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/** 
+ *
+ * main function - an entry to the c program
+ *
+ * puts -  displays a char to the std output
+ * 
+ * Return - returns 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	
+	return(0);
+}
