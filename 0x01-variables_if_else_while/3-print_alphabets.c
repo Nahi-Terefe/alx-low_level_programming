5dedc28caafd7cd47bd331962912c387cb21a660
Nahi-Terefe <nahi.terefe@gmail.com>
2021-08-30 16:40:30 +0300

    add 3-print_alphabets secript

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -	entry to c program
+ *
+ * Desc:	this program will print all letter
+ *			of english lang bothe upper and lower case
+ *
+ * Return:	returns zero
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	/*putchar('\n');*/
+
+	return (0);
+}
