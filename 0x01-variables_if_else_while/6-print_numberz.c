d1d1186b197863ea9b76ab92da77be5ea729c8eb
Nahi-Terefe <nahi.terefe@gmail.com>
2021-08-30 18:09:57 +0300

    add 6-print_numberz secript

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 /**
+ * intToAscii - function returns Ascii values of the given integer
+ * num:	parameter passed to the function which is type int
+ * Return:	ascii values of the given number
+ */
+char intToAscii(int num)
+{
+	return ('0' + num);
+}
+
+/**
  * main -	entry to c program
  * Desc:	this program will print single digits of base 10 number
  *			using only putchar() function.
@@ -10,11 +20,6 @@ int main(void)
 {
 	int num;
 
-	/* intToAscii - function returns Ascii values of the given integer */
-	char intToAscii(int num)
-	{
-		return ('0' + num);
-	}
 	for (num = 0; num < 10; num++)
 	{
 		putchar(intToAscii(num));
