ddb3027089129e62fcf1cc7f7202b1e0233a939b
Nahi-Terefe <nahi.terefe@gmail.com>
2021-08-30 16:04:14 +0300

    add 2-print_alphabet secript

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -	entry to c program
+ *
+ * Desc:	this program will print out all small letter of english in the stdio
+ *
+ * Return:	returns zero
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+}
