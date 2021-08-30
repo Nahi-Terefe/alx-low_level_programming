a17ee86e70f5ecf64e64fe59822bdaceafdbeabb
Nahi-Terefe <nahi.terefe@gmail.com>
2021-08-30 20:55:27 +0300

    add 100-print_comb3 secript

---
+++ 0x01-variables_if_else_while/100-print-comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main -	entry to c program
+ * Desc:	this program will print out all possible different 
+ *			combinations of two digits.
+ * Return:	returns zero
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+
+		for (int j = num + 1; j <= '9'; j++)
+		{
+			putchar(num);
+			putchar(j);
+
+			if (num != '8' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
