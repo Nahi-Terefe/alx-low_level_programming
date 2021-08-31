fc6fd35607452510a37ea17050558df74c4f825f
Nahi-Terefe <nahi.terefe@gmail.com>
2021-08-31 12:59:46 +0300

    add 0-holberton.c secript

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "_putchar.c"
+
+/**
+ * main - entry to c program
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char school_name[] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(school_name[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+## Project 0x02_functions_nested_loops 
+
+
+Description of the project and each task comming soon...not yet:)
