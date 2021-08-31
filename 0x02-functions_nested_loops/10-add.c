179f963844cf051afd8dd82880c09045e110abf0
Nahi-Terefe <nahi.terefe@gmail.com>
2021-08-31 21:05:49 +0300

    add 10-add.c secript

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - addes two integers.
+ * @i: param accepts int.
+ * @j: param accepts int.
+ * Return: returns sum result.
+ */
+int add(int i, int j)
+{
+	int sum = i + j;
+
+	return (sum);
+}
