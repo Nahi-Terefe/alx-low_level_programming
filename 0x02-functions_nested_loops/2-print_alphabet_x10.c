08c4f0ca252194932f6ea7b7d46f589e19667e82
Nahi-Terefe <nahi.terefe@gmail.com>
2021-08-31 14:02:23 +0300

    add 2-print_alphabet_x10.c secript

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints out small letter alphabet 10 times.
+ *
+ * Return: Always 0 (sucsess).
+ */
+void print_alphabet_x10(void)
+{
+	char letters;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (letters = 'a'; letters <= 'z'; letters++)
+		{
+			_putchar(letters);
+		}
+	}
+	_putchar('\n');
+}
