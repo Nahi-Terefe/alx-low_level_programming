31090b4aa58177884185ed4a02001d8805a847f8
Nahi-Terefe <nahi.terefe@gmail.com>
2021-08-31 17:27:12 +0300

    add 7-print_last_digit.c secript

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - compute and return the last digit of any int.
+ * @n: peram accepts any int data type.
+ * Return: returns the last digit of given int n.
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
