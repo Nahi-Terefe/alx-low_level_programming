504e3f032b2845303772135bedb3beb4c1aaa587
Nahi-Terefe <nahi.terefe@gmail.com>
2021-08-31 13:54:24 +0300

    add 1-alphabet.c secript

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * main - call and excute the print_alphabet function.
+ * print_alphabet - prints out small letter alphabet.
  *
  * Return: Always 0 (sucsess).
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
+	char letters;
 
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		_putchar(letters);
+	}
 	_putchar('\n');
-	return (0);
 }
