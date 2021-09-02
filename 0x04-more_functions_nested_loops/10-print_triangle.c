5041852bb2479636c0b34deb68e3dad3d5f79192
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-02 22:04:42 +0300

    add 10-print_triangle.c secript

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints out tirangular shape based on size using #.
+ *@size: accepts any int.
+ * Return: void.
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				putchar(' ');
+			}
+
+			for (k = 1; k <= i; k++)
+			{
+				putchar('#');
+			}
+
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
