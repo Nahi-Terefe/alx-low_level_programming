c29309e4cdc5edee9a47355620bafd97ed96fc24
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-02 16:59:26 +0300

    add 4-print_alphabts secript

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints out all single digit number except 2 and 4
+ * print_numbers - prints out all single digit number.
  *
  * Return: void.
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 || i != 4)
-		{
-			_putchar(i + '0');
-		}
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * print_numbers - check and identify if a given char in upper case or not
+ * print_most_numbers - prints out all single digit number except 2 and 4
  *
  * Return: void.
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i + '0');
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }
