1274d3a39071760e5fb9ee932cb344d546606944
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-03 01:08:56 +0300

    add 100-prime_factor.cc secript

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
     long i, j;
     long num = 612852475143;
     long largest_prime = 1;
+    int isprime;
 
-    for (i = 2; i <= num / 2; i++)
+    for (i = 2; i <= num; i++)
     {
         /* check if the number is divisible by i */
         if (num % i == 0)
         {
 
+            isprime = 1;
+
             /* check if the devisor is prime */
-            for (j = 2; j <= (i / 2); j++)
+            for (j = 2; j <= i / 2; j++)
             {
 
                 if (i % j == 0)
                 {
+                    isprime = 0;
                     break;
                 }
-                else if (i > largest_prime)
-                {
-                    largest_prime = i;
-                }
+            }
+            if (isprime == 1)
+            {
+                printf("%li ", i);
+
+                largest_prime = i;
             }
         }
     }
 
-    printf("%ld \n", largest_prime);
+    printf("\nlargest prime: %ld \n", largest_prime);
 
     return (0);
 }
