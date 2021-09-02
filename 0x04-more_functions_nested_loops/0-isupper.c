3d5d88365606b45e00de37b0bc51eeb9e0799abe
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-02 15:47:05 +0300

    add 0-isupper.c script

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  * _isupper - check and identify if a given char in upper case or not
- * 
+ * @c: accepts value of a char.
  * Returns: 1 if upper and 0 if not
  */
 int _isupper(int c)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-int 0-isupper(int c);
-
+int _isupper(int c);
 
 #endif /* MAIN_H */
