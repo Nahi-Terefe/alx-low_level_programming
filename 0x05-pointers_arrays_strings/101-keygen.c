71b4e403caccdf168d23e13d43b431f632101356
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-06 23:43:32 +0300

    add 101-keygen.c secript

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -10,22 +9,34 @@
  */
 int main(void)
 {
-	char seed[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char password[22];
-	int i, n, pass, c;
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
-	srand(time(0));
-
-	for (i = 0; pass < 2772; i++)
+	srand(time(NULL));
+	while (s != 2772)
 	{
-		n = rand() % 10;
-		password[i] = seed[n];
-		pass += password[i];
+		i = k = s = 0;
+		while ((2772 - 122) > s)
+		{
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
+		}
+		while (c[k])
+		{
+			if (c[k] == (2772 - s))
+			{
+				p[i] = c[k];
+				s += c[k];
+				i++;
+				break;
+			}
+			k++;
+		}
 	}
-
-	c = 2772 - pass;
-	password[i] = c;
-	printf("%s\n", password);
-
+	p[i] = '\0';
+	printf("%s", p);
 	return (0);
 }
