c6c8aae56cc6c4bf731669fa81fadf67e6a1f5d5
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-06 22:33:34 +0300

    add 4-print_rev.c secript

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,16 @@
 void print_rev(char *s)
 {
 	int i = 0;
+	int j;
 
-	int len_str = _strlen(s);
-
-	for (i = len_str - 1; i >= 0; i--)
+	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; i--)
+	{
+		_putchar(s[j]);
 	}
 	_putchar('\n');
 }
