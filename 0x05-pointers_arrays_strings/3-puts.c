25958b749c676d76a3ec906309d4e245cec07e22
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-06 21:54:16 +0300

    add 3-puts.c secript

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string to stdout but character wise.
+ * @str: pointer which accepts address of a string value.
+ * Return: nothing.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+}
