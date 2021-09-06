26a702ce6d26b116834b1d58a86ec51a0d45f5ad
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-06 23:20:12 +0300

    add 8-print_array.c secript

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,22 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_array - prints n elements of an array of integers, followed by a new line.
- *@a: pointer parameter
+ * print_array - prints n elements of an array of jgers, followed by a new line.
+ *@a: pojr parameter
  *@n: int parameter 
  * Return: Always 0 (Success)
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", *(a + i));
-		if (i < (n - 1))
-		{
+
+		j = *(a + i);
+
+		printf("%d", j);
+
+		if (i != n - 1)
 			printf(", ");
-		}
 	}
+
 	printf("\n");
 }
