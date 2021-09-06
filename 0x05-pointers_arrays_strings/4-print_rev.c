0fd0d1587b5b2ab9051ecaad916667bb6afc5e80
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-06 22:26:54 +0300

    add 4-print_rev.c secript

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints a string to stdout in reverse.
+ * @str: pointer which accepts address of a string value.
+ * Return: nothing.
+ */
+void print_rev(char *str)
+{
+	int i = 0;
+
+	int len_str = _strlen(*str);
+
+	for (i = len_str - 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
