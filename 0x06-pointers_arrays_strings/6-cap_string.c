220dd2270272395b4fc7721157adb86bd6b4e1ca
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-09 22:11:00 +0300

    add 6-cap_string.c script

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,22 +7,28 @@
  */
 char *cap_string(char *s)
 {
+
+	char separator[] = " \t\n,;.!?\"(){}";
 	int i, j;
-	char c[] = " \t\n,;.!\?\"(){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; c[j] != '\0'; j++)
+
+		if (s[i] >= 's' && s[i] <= 'z')
 		{
-			if (s[i] == c[j])
+
+			if (i == 0)
+				s[i] -= 32;
+
+			else
 			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				for (j = 0; separator[j] != '\0'; j++)
 				{
-					s[i + 1] = s[i + 1] - 32;
+					if (s[i - 1] == separator[j])
+						s[i] -= 32;
 				}
 			}
 		}
 	}
-
 	return (s);
 }
