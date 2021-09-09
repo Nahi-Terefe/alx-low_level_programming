40b6b1114b460c7558f67673ee499b9e3dc91aa8
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-09 18:49:01 +0300

    add 2-strncpy.c script

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,19 +9,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
 	int j;
-
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		dest[j] = src[j];
 	}
-
-	dest[i + j] = '\0';
+	for (; j < n; j++)
+	{
+		dest[j] = '\0';
+	}
 
 	return (dest);
 }
