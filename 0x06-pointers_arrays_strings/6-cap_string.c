f47cbe9749b27c0b517e8f6ff8b1602cc602272a
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-09 21:13:11 +0300

    add 6-cap_string.c script

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - captilize first letter of all words in a string.
+ * @s: string pointer
+ * Return: a string.
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	char c[] = " \t\n,;.!\?\"(){}";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (s[i] == c[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
