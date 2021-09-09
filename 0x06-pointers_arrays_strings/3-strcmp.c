6f85092257a29292def6ab4e0f42d184c3456dd8
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-09 19:36:09 +0300

    add 3-strcmp.c script

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,24 +6,16 @@
  * @s2: accepts source string.
  * Return: pointer to the destination string.
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int flag = 0;
 
-	while (s1 != '\0' && s2 != '\0')
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
-			flag = 1;
-			break;
 		}
-
-		i++;
 	}
-	if (flag == 0)
-	{
-		return (0);
-	}
+	return (0);
 }
