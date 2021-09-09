148c6a3aba919b9c82f47f3973b7a4c48de78a56
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-09 19:05:08 +0300

    add 3-strcmp.c script

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings.
+ * @s1: accepts destination string.
+ * @s2: accepts source string.
+ * Return: pointer to the destination string.
+ */
+int *_strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+
+	for (i = 0; s1[i] != '\0', i++)
+	{
+	}
+
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+
+	if (i == j)
+	{
+		return (0);
+	}
+	else if (i > j)
+	{
+		return (15);
+	}
+	else
+	{
+		return (-15);
+	}
+}
