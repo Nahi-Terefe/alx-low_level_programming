895f897a1206db8f4ca11b2c12722d8c0d4221d1
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-09 22:23:36 +0300

    add 100-rot13.c script

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * rot13 - rotate a char by 13 place and subtitute with 13th char.
+  * @s: string pointer
+  * Return: a string.
+  */
+char *rot13(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+		{
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
+			{
+				s[a] -= 13;
+				break;
+			}
+
+			s[a] += 13;
+			break;
+		}
+
+		a++;
+	}
+
+	return (s);
+}
