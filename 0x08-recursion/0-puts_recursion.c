5ecfb3b071146e08daaeab69c0b3745d2971fb22
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-15 14:18:11 +0300

    add 0-puts_recursin.c script

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line recursivly.
+ *@s: pointer to char.
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+		return;
+	}
+}
+
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,4 @@
+## project 0x08-recursion
+
+
+description of the project and all task in the project...
