26188101d15cb6e8d0446c0774e35c632a08ac30
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-15 17:53:31 +0300

    add 2-strlen_recursion.c script

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,13 +7,14 @@
  */
 int _strlen_recursion(char *s)
 {
-	static int i;
 
 	if (*s != '\0')
 	{
-		i++;
-		_strlen_recursion(s + 1);
-	}
 
-	return (i);
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
 }
