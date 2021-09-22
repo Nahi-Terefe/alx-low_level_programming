6ffde258fceb0c74b57801d25478659f2f5330b5
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-22 22:02:12 +0300

    add 1-strdup.c script

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * *_strdup -  creates an array of chars,
+ *					and initializes it with a specific char.
+ * @str: string to be duplicated.
+ * Return: a pointer or NULL.
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int str_size = strlen(str);
+
+	if (str_size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		char *dup_str = (char *)malloc(str_size * sizeof(char));
+
+		if (dup_str == NULL)
+		{
+			return (NULL);
+		}
+
+		for (i = 0; i < str_size; i++)
+		{
+			dup_str[i] = str[i];
+		}
+
+		return (dup_str);
+	}
+}
