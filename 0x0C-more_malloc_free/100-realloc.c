f15ead9fabf73360bea4b683c967406b944c5e18
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-23 22:30:14 +0300

    add 100-realloc.c script

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _realloc -   reallocates a memory block using malloc and free.
+ * @ptr: pointer to the memory previusly
+ * @old_size: old size
+ * @new_size: new size
+ * Return: Always 0.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *oldptr = ptr, *newp; /* oldptr- desreference newp - memory space */
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (oldptr); /* ptr */
+	if (oldptr == NULL)
+	{
+		newp = malloc(new_size);
+		return (newp);
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	newp = malloc(new_size);
+	if (newp == NULL) /* malloc fails?*/
+		return (NULL);
+	for (i = 0; i < old_size && i < new_size; i++) /*check i bound to sizes*/
+		newp[i] = oldptr[i];
+	free(ptr);
+	return (newp);
+}
