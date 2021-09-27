e93b5f88b292f888f6279b12941695e80e147b3d
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-28 00:21:31 +0300

    add 2-print_dog.c script

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,19 +17,9 @@ void print_dog(struct dog *d)
 		if (!d->owner || strcmp(d->owner, "") == 0)
 			d->owner = null_variable;
 
-		if (d->age <= 0)
-		{
-			printf("Name: %s\nAge: %s\nOwner: %s\n",
-				   d->name,
-				   null_variable,
-				   d->owner);
-		}
-		else
-		{
-			printf("Name: %s\nAge: %f\nOwner: %s\n",
-				   d->name,
-				   d->age,
-				   d->owner);
-		}
+		printf("Name: %s\nAge: %f\nOwner: %s\n",
+			   d->name,
+			   d->age,
+			   d->owner);
 	}
 }
