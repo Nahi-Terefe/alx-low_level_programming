4c80cdeaee3ec7432b479362cab4d8ee8528d94e
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-27 23:38:50 +0300

    add 1-init_dog.c script

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,9 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-
-	return;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
