8127a808ccbdc5ad700faf4ac4ff40f6fa0e6927
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-30 00:58:23 +0300

    add 2-int_index.c script

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - function that searches for an integer,
+  *
+  * @array: name.
+  * @size: pointer.
+  *@cmp: pointer.
+  * Return: nothing.
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == 0 || size <= 0 || cmp == 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
