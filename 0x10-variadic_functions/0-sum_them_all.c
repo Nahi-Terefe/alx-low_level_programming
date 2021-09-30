6f97a755020bdf9adc74f90376fc17859b6db1a1
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-30 22:41:11 +0300

    add sum_them_all.c script

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters.
+ * @d: dog instance
+ * @name: name of dog
+ * @age: age of dog.
+ * @owner: owner of the dog.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return sum;
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,5 @@
+## project 0x10-variadic_functions
+
+
+
+description of the project and each task.
