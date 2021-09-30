937e52896a0f673a2dcdbe5638e39998d561947f
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-30 23:16:17 +0300

    add 1-print_numbers.c script

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(args, int));
 		if (separator != 0 && i < n - 1)
 		{
-			printf("%s ", separator);
+			printf("%s", separator);
 		}
 	}
 
