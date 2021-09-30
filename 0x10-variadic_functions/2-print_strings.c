b60adf5bb487272e1e47c52b372a8ac73b3ff7bb
Nahi-Terefe <nahi.terefe@gmail.com>
2021-09-30 23:34:27 +0300

    add 2-print_strings.c script

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,7 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	char *nil = "nil";
+	char *nil = "(nil)";
 	char *arg_strings = "";
 
 	va_start(args, n);
