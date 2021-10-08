32bbf9114f999bd36bf024104eda197f2f1ee624
Nahom Terefe <nahi.terefe@gmail.com>
2021-10-08 11:33:47 +0300

    add 1-list_len.c script

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,21 +6,12 @@
  * @h: pointer to a struct called list_t
  * Return: the number of nodes.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	int num_of_node = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-
 		h = h->next;
 		num_of_node++;
 	}
