df61b2d3bdf740fa0b36e1787812ea3501578c0a
Nahom Terefe <nahi.terefe@gmail.com>
2021-10-12 23:10:42 +0300

    add 103-find_loop.c file

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,31 +8,15 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current_head;
-
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (head == NULL)
-	{
 		return (NULL);
-	}
-
-	while (head != NULL)
+	for (i = 0; i < index; i++)
 	{
-		if (i == index)
-		{
-			current_head = head;
-			break;
-		}
-		else if (i > index)
-		{
-			current_head = NULL;
-			break;
-		}
-
 		head = head->next;
-		i++;
+		if (head == NULL)
+			return (NULL);
 	}
-
-	return (current_head);
+	return (head);
 }
