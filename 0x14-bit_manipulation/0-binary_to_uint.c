eeb996f0cd6f1cd5dbbbb44f5be6a21ca5e26d55
Nahom Terefe <nahi.terefe@gmail.com>
2021-10-14 21:51:45 +0300

    add 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,18 +12,18 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int decimal = 0;
 	unsigned int multiplayer = 1;
 	int i, j;
-	
+
 	if (b == NULL)
 		return (0);
 	for (i = 0; b[i]; i++)
 	{
 	}
-	
-	for (j = i -1; j >= 0; j--)
+
+	for (j = i - 1; j >= 0; j--)
 	{
 		if (b[j] != '1' && b[j] != '0')
 			return (0);
-		
+
 		if (b[j] == '1')
 		{
 			decimal += multiplayer;
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,39 +1,31 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary to unsigned int.
+ * print_binary - converts a binary to unsigned int.
  *
  * @b: pointer to a string.
  *
  * Return: int or 0(fail)
  */
 void print_binary(unsigned long int n)
-{
-	unsigned long int i;
+{ 
+	int i, j;
+	int flag = 0;
 
-	/**
-	 * assumption: the system is 32 bit and a int value will 
-	 * take 32 bit space in the memory.
-	 *
-	 * i is initialized with one bit shifted 
-	 * to the left 31 times, and then it shifted 
-	 * one bit to the right util becomes less that
-	 * zero. and in each i teration the bit compared wit
-	 * n bitwisly using the & operator. 
-	 *
-	 * so to do the bit wise operation the system automaticaly 
-	 * converts n in to binary and compare the 
-	 * corosponding i bit with n bit in each iteration.
-	 */   
-	for (i = (1 << 31); i > 0; (i >> 1))
+	if (n == 0)
+		_putchar('0');
+	for (i = 63; i >= 0; i--)
 	{
-		if (n & i)
+		j = n >> i;
+		if (j & 1)
 		{
-			printf("1");
+			_putchar('1');
+			flag = 1;
 		}
 		else
 		{
-			printf("0");
+			if (flag == 1)
+				_putchar('0');
 		}
 	}
 }
