8d28fbf0a11dca4503a72d8077e5480667043ce3
Nahom Terefe <nahi.terefe@gmail.com>
2021-10-14 22:15:45 +0300

    add 2-get_bit.c file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary to unsigned int.
+ * get_bit - function that returns the value of a bit at a given index.
+ * @n: number to manipulate.
+ * @index: integer
  *
- * @b: pointer to a string.
- *
- * Return: int or 0(fail)
+ * Return: 1 or 0 or -1(fail)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int i;
-	
+
 	if (n & (1 << index))
 	{
 		return (1);
@@ -21,7 +20,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	}
 	else
 	{
-		return (-1);	
+				return (-1);
 	}
-	
+
 }
