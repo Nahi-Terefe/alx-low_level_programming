653bd1d1174e210edd7331da1c647ed5bcaebed2
Nahom Fikadu <nahi.terefe@gmail.com>
2021-11-26 01:57:05 +0300

    Update 0-print_distint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int i = 0;
-	dlistint_t *next_head;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		h == h->next;
+		h = h->next;
 		i++;
 	}
 
